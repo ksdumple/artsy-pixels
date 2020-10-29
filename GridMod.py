b3ca427b146f3357027b63b8d834ed7705f29fd4
ksdumple <73575933+ksdumple@users.noreply.github.com>
2020-10-30 07:17:38 +1100

    Add files via upload

---
+++ GridMod.py
@@ -0,0 +1,194 @@
+import pygame
+pygame.init()
+
+
+class grid(object):
+    def __init__(self, win, width, height, cols, rows, showGrid=False, startx = 0, starty = 0, bg=(255,255,255)):
+        self.width = width
+        self.height = height
+        self.cols = cols
+        self.rows = rows
+        self.bg = bg
+        self.startx = startx
+        self.starty = starty
+        self.lineThick = 1
+        self.showGrid = showGrid 
+        self.isSelected = None
+        self.grid = None
+
+        self.screen = win
+        pygame.display.update()
+                    
+    def getGrid(self):
+        return self.grid 
+
+    def drawGrid(self, lineColor=(0,0,0)): 
+        x = self.startx
+        y = self.starty
+        
+        for i in range(self.cols):
+            y = self.starty + self.height
+            if i > 0:
+                x += (self.width / self.cols)
+            for j in range(self.rows):
+                y -= self.height / self.rows 
+                pygame.draw.rect(self.screen, (0,0,0),(x, y, self.width / self.cols, self.height/ self.rows), 1)
+
+    def clicked(self, pos): 
+        try:
+            t = pos[0]
+            w = pos[1]
+            g1 = int((t - self.startx) / self.grid[0][0].w)
+            g2 = int((w - self.starty) / self.grid[0][0].h)
+
+            self.selected = self.grid[g1][g2]
+
+            return self.grid[g1][g2]
+        
+        except IndexError: 
+            return False
+
+    def isSelected(self): 
+        return self.selected
+
+
+class pixelArt(grid):  
+    def drawGrid(self):
+        self.grid = []
+        for i in range(self.cols):
+            self.grid.append([])
+            for j in range(self.rows):
+                self.grid[i].append(pixel(i, j, self.width, self.height, self.cols, self.rows, self.startx, self.starty, self.showGrid))
+                self.grid[i][j].show(self.screen, (255,255,255), self.lineThick)
+                if self.showGrid:
+                    self.grid[i][j].show(self.screen, (0,0,0), 1,False,True)
+                    
+        
+        for c in range(self.cols):
+            for r in range(self.rows):
+                self.grid[c][r].getNeighbors(self.grid)
+        
+        self.selected = self.grid[self.cols - 1][self.rows - 1]
+
+        
+    def clearGrid(self): 
+        for pixels in self.grid:
+            for p in pixels:
+                if self.showGrid: 
+                    p.show(self.screen, self.bg, 0)
+                    p.show(self.screen, (0,0,0), 1)
+                else:
+                    p.show(self.screen, self.bg, 0)
+
+
+
+class colorPallet(pixelArt):
+    def setColor(self, colorList): 
+        colourCount = 0
+
+        for pixels in self.getGrid():
+            for p in pixels:
+                p.show(self.screen, colorList[colourCount],0)
+                colourCount += 1
+
+class menu(grid):
+    def setText(self, textList): 
+        
+        self.grid = []
+        for i in range(self.cols):
+            self.grid.append([])
+            for j in range(self.rows):
+                self.grid[i].append(textObject(i, j, self.width, self.height, self.cols, self.rows, self.startx, self.starty))
+        c = 0
+        for spots in self.getGrid():
+            for s in spots:
+                s.showText(self.screen, textList[c])
+                c += 1
+
+                 
+class textObject():
+    def __init__(self, i, j, width, height, cols, rows, startx=0, starty=0):
+        self.col = i 
+        self.row = j 
+        self.rows = rows 
+        self.cols = cols 
+        self.w = width / cols
+        self.h = height / rows
+        self.x = self.col * self.w + startx
+        self.y = self.row * self.h + starty
+        self.text = ''
+    
+    def showText(self, win, txt): 
+        self.text = txt
+        myFont = pygame.font.SysFont('arial', 15)
+        text = myFont.render(self.text, 1, (0,0,0))
+        win.blit(text, (self.x + (self.w /2 - text.get_width() / 2), self.y + (self.h/2 - text.get_height() / 2))) 
+
+    def show(self, screen, color, st, outline=False): 
+        pygame.draw.rect(screen, color, (self.x, self.y, self.w, self.h), st)
+
+
+class pixel():
+    def __init__(self, i,j, width, height, cols, rows, startx=0, starty=0, showGrid=False):
+        self.col = i 
+        self.row = j 
+        self.color = (255,255,255)
+        self.rows = rows 
+        self.cols = cols 
+        self.showGrid = showGrid
+        self.w = width / cols
+        self.h = height / rows
+        self.x = self.col * self.w + startx
+        self.y = self.row * self.h + starty
+        self.neighbors = [] 
+        
+    def show(self, screen, color, st, outline=False, first=False): 
+        if not(first):
+            self.color = color
+            
+        pygame.draw.rect(screen, color, (self.x, self.y, self.w, self.h), st)
+        if self.showGrid and not(outline):
+            pygame.draw.rect(screen, (0,0,0), (self.x, self.y, self.w, self.h), 1)
+        
+    def getPos(self):
+        return (self.col * self.w, self.row * self.h)
+
+    def click(self, screen, color): 
+        self.show(screen, color, 0)
+        self.color = color
+
+    def getColor(self):
+        return self.color
+
+    def getNeighbors(self, grid):
+        i = self.col 
+        j = self.row 
+        rows = self.rows
+        cols = self.cols
+
+        
+        if i < cols-1: 
+            self.neighbors.append(grid[i + 1][j])
+        if i > 0: 
+            self.neighbors.append(grid[i - 1][j])
+        if j < rows-1: 
+            self.neighbors.append(grid[i][j + 1])
+        if j > 0 : 
+            self.neighbors.append(grid[i][j - 1])
+
+            
+         
+        if j > 0 and i > 0: 
+            self.neighbors.append(grid[i - 1][j - 1])
+
+        if j + 1 < rows and i > -1 and i - 1 > 0: 
+            self.neighbors.append(grid[i - 1][j + 1])
+
+        if j - 1 < rows and i < cols - 1 and j - 1 > 0: 
+            self.neighbors.append(grid[i + 1][j - 1])
+            
+        if j < rows - 1 and i < cols - 1: 
+            self.neighbors.append(grid[i + 1][j + 1])
+        
+    def neighborsReturn(self):
+        return self.neighbors 
---
+++ artsypixels.py
@@ -0,0 +1,392 @@
+import pygame
+from tkinter import *
+from tkinter import messagebox
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+import tkinter.font as font
+import GridMod
+from GridMod import colorPallet
+from GridMod import pixelArt
+from GridMod import menu
+from GridMod import grid
+import sys
+import time
+
+sys.setrecursionlimit(1000000)
+
+pygame.init() #initalize pygame
+paintBrush = pygame.image.load("logo.png")
+currentVersion = 1.1
+
+rows = 50
+cols = 50
+wid = 900
+height = 600
+
+checked = []
+def fill(spot, grid, color, c):
+   if spot.color != c:
+      pass
+   else:
+      spot.click(grid.screen, color)
+      pygame.display.update()
+      
+      i = spot.col 
+      j = spot.row 
+
+      
+      if i < cols-1: 
+         fill(grid.getGrid()[i + 1][j], grid, color, c)
+      if i > 0: 
+         fill(grid.getGrid()[i - 1][j], grid, color, c)
+      if j < rows-1: 
+         fill(grid.getGrid()[i][j + 1], grid, color, c)
+      if j > 0 : 
+         fill(grid.getGrid()[i][j - 1], grid, color, c)
+      
+
+def save(cols, rows, show, grid, path):
+   if len(path) >= 4:
+      if path[-4:] != '.txt':
+         path = path + '.txt'
+   else:
+      path = path + '.txt'
+
+   file = open(path, 'w')
+   file.write(str(cols) + ' ' + str(rows) + ' ' + str(show) +'\n')
+
+   for pixel in grid:
+       for p in pixel: 
+           wr = str(p.color[0]) + ',' + str(p.color[1]) + ',' + str(p.color[2])
+           file.write(wr + '\n')
+   file.write(str(currentVersion))
+
+   file.close()
+   name = path.split("/")
+   changeCaption(name[-1])
+
+
+def openFile(path):
+    global grid
+    
+    file = open(path, 'r')
+    f = file.readlines()
+    if f[-1] == str(currentVersion):
+       
+       dimensions = f[0].split() 
+       columns = int(dimensions[0])
+       rows = int(dimensions[1])
+       
+       if dimensions[2] == '0': 
+          v = False
+       else:
+          v = True
+       initalize(columns, rows, v)  
+       name = path.split("/")
+       changeCaption(name[-1])
+       
+       line = 0
+       for i in range(columns): 
+          for j in range(rows):
+             line += 1
+             nColor = []
+             for char in f[line].strip().split(','):
+                nColor.append(int(char))
+                
+             
+             grid.getGrid()[i][j].show(win, tuple(nColor), 0) 
+    else:
+      window = Tk()
+      window.withdraw()
+      messagebox.showerror("Unsupported Version", "The file you have opened is not supported in this version.")
+
+
+def changeCaption(txt):
+   pygame.display.set_caption(txt)
+          
+
+
+def showFileNav(op=False):
+    window = Tk()
+    window.attributes("-topmost", True)
+    window.withdraw()
+    myFormats = [('Windows Text File','*.txt')]
+    if op:
+       filename = askopenfilename(title="Open File",filetypes=myFormats) # Ask the user which file they want to open
+    else:
+       filename = asksaveasfilename(title="Save File",filetypes=myFormats) # Ask the user choose a path to save their file to
+       
+    if filename: 
+       x = filename[:] 
+       return x
+
+
+def onsubmit(x=0):
+    global cols, rows, wid, heigh
+    
+    st = rowsCols.get().split(',') 
+    window.quit()
+    window.destroy()
+    try: 
+        if st[0].isdigit(): 
+            cols = int(st[0])
+            while 600//cols != 600/cols:
+               if cols < 300:
+                  cols += 1
+               else:
+                  cols -= 1
+        if st[1].isdigit():
+            rows = int(st[1])
+            while 600//rows != 600/rows:
+               if rows < 300:
+                  rows += 1
+               else:
+                  rows -= 1
+        if cols > 300:
+          cols = 300
+        if rows > 300:
+          rows = 300
+
+    except:
+        pass
+
+def updateLabel(a, b, c):
+   sizePixel = rowsCols.get().split(',') 
+   l = 12
+   w = 12
+   
+   try:
+      l = 600/int(sizePixel[0])
+   except:
+      pass
+
+   try:
+      w = 600/(int(sizePixel[1]))
+   except:
+      pass
+
+   label1.config(text='Pixel Size: ' + str(l) + ', ' + str(w)) 
+
+
+
+def initalize(cols, rows, showGrid=False):
+   global pallet, grid, win, tools, lineThickness, saveMenu
+
+   try:
+      del grid
+   except:
+      pass
+   
+   pygame.display.set_icon(paintBrush)   
+   win = pygame.display.set_mode((int(wid), int(height) + 100))
+   pygame.display.set_caption('Artsy Pixels')
+   win.fill((255,255,255))
+
+
+
+   grid = pixelArt(win, int(wid), int(height), cols, rows, showGrid)
+   grid.drawGrid()
+
+   pallet = colorPallet(win, 90, 90, 3, 3, True, 10, grid.height + 2)
+   pallet.drawGrid()
+
+   colorList = [(0,0,0), (255,255,255), (255,0,0), (0,0,255), (186,255,201), (255,179,186), (186,225,255), (255,255,186), (255,223,186)]
+   pallet.setColor(colorList)
+
+   tools = menu(win, 300, 40, 5, 1, True, grid.width - 310, grid.height + 50)
+   tools.drawGrid()
+
+   buttons = ['Draw', 'Erase', 'Fill', 'Replace', 'Clear']
+   tools.setText(buttons)
+   tools.drawGrid()
+
+   l = tools.getGrid()
+   l[0][0].show(grid.screen, (255,0,0),1, True)
+
+   lineThickness = menu(win, 180, 40, 4, 1, True, grid.width - 250, grid.height + 10)
+   lineThickness.drawGrid()
+
+   buttons = ['1', '2', '3', '4']
+   lineThickness.setText(buttons)
+
+   saveMenu = menu(win, 140, 40, 2, 1, True, grid.width - 600, grid.height + 25)
+   saveMenu.drawGrid()
+
+   buttons = ['Save', 'Open']
+   saveMenu.setText(buttons)
+
+   pygame.display.update()
+
+
+window = Tk()
+window.title('Artsy Pixels')
+
+t_var = StringVar()
+t_var.trace('w', updateLabel)
+
+label = Label(window, text='# of Rows and Columns (e.g: 25,50): ')
+rowsCols = Entry(window, textvariable=t_var)
+
+label1 = Label(window, text="Pixel Size: 12.0, 12.0")
+var = IntVar()
+c = Checkbutton(window, text="View Grid", variable=var)
+submit = Button(window, text='Submit', command=onsubmit)
+window.bind('<Return>', onsubmit)
+
+submit.grid(columnspan=1, row=3, column=1,pady=2)
+c.grid(column=0, row=3)
+label1.grid(row=2)
+rowsCols.grid(row=0, column=1, pady=3, padx=8)
+label.grid(row=0, pady=3)
+
+window.update()
+mainloop()
+
+
+
+initalize(cols, rows, var.get())
+pygame.display.update()
+color = (0,0,0) 
+thickness = 1
+replace = False
+doFill = False
+savedPath = '' 
+
+run = True
+while run:
+    ev = pygame.event.get()
+
+    for event in ev:
+        if event.type == pygame.QUIT:
+            window = Tk()
+            window.withdraw()
+
+            if pygame.display.get_caption()[0].count('*') > 0: 
+               if messagebox.askyesno("Saying Goodbye?", "Would you like to save before closing?"):
+                  if savedPath != "":
+                     save(cols, rows, grid.showGrid, grid.getGrid(),savedPath)
+                  else:
+                     path = showFileNav()
+                     if path:
+                        savedPath = path
+                        save(cols, rows, grid.showGrid, grid.getGrid(),savedPath)
+            run = False
+         
+        if pygame.mouse.get_pressed()[0]: 
+            try:
+                pos = pygame.mouse.get_pos()
+                if pos[1] >= grid.height: 
+                    if pos[0] >= tools.startx and pos[0] <= tools.startx + tools.width and pos[1] >= tools.starty and pos[1] <+ tools.starty + tools.height: #If the mouse ic clicking on the tools grid
+                        replace = False
+                        doFill = False
+                        tools.drawGrid() 
+                        buttons = ['Draw', 'Erase', 'Fill', 'Replace', 'Clear']
+                        tools.setText(buttons)
+                        
+                        clicked = tools.clicked(pos)
+                        clicked.show(grid.screen, (255,0,0), 1, True)
+
+
+                        if clicked.text == 'Draw': 
+                            color = (0,0,0)
+                        elif clicked.text == 'Erase':
+                            color = (255,255,255)
+                        elif clicked.text == 'Fill':
+                            doFill = True
+                        elif clicked.text == 'Replace':
+                            replace = True
+                        elif clicked.text == 'Clear':
+                            grid.clearGrid()
+                            tools.drawGrid() 
+                            buttons = ['Draw', 'Erase', 'Fill', 'Replace', 'Clear']
+                            tools.setText(buttons)
+                            l = tools.getGrid()
+                            l[0][0].show(grid.screen, (255,0,0),1, True)
+                            
+
+                    elif pos[0] >= pallet.startx and pos[0] <= pallet.startx + pallet.width and pos[1] >= pallet.starty and pos[1] <= pallet.starty + pallet.height:
+                        clicked = pallet.clicked(pos)
+                        color = clicked.getColor() 
+
+                        pallet = colorPallet(win, 90, 90, 3, 3, True, 10, grid.height + 2)
+                        pallet.drawGrid()
+
+                        colorList = [(0,0,0), (255,255,255), (255,0,0), (0,0,255), (186,255,201), (255,179,186), (186,225,255), (255,255,186), (255,223,186)]
+                        pallet.setColor(colorList)
+                        clicked.show(grid.screen, (255,0,0), 3, True)
+                        
+                    elif pos[0] >= lineThickness.startx and pos[0] <= lineThickness.startx + lineThickness.width and pos[1] >= lineThickness.starty and pos[1] <= lineThickness.starty + lineThickness.height:
+                        lineThickness.drawGrid() 
+                        buttons = ['1', '2', '3', '4']
+                        lineThickness.setText(buttons)
+                        
+                        clicked = lineThickness.clicked(pos)
+                        clicked.show(grid.screen, (255,0,0), 1, True)
+
+                        thickness = int(clicked.text) 
+
+
+                    elif pos[0] >= saveMenu.startx and pos[0] <= saveMenu.startx + saveMenu.width and pos[1] >= saveMenu.starty and pos[1] <= saveMenu.starty + saveMenu.height:
+                        clicked = saveMenu.clicked(pos)
+
+                        if clicked.text == 'Save': 
+                            path = showFileNav()
+                            if path:
+                               savedPath = path
+                               save(cols, rows, grid.showGrid, grid.getGrid(),savedPath)
+                        else: 
+                            path = showFileNav(True)
+                            if path:
+                               openFile(path)
+                               savedPath = path
+
+                            
+                else:
+                    if replace: 
+                        tools.drawGrid() 
+                        buttons = ['Draw', 'Erase', 'Fill', 'Replace', 'Clear']
+                        tools.setText(buttons)
+                        
+                        tools.getGrid()[0][0].show(grid.screen, (255,0,0), 1, True)
+
+                        clicked = grid.clicked(pos)
+                        c = clicked.color
+                        replace = False
+
+                        for x in grid.getGrid():
+                            for y in x:
+                                if y.color == c:
+                                    y.click(grid.screen, color)
+                    elif doFill:
+                       clicked = grid.clicked(pos)
+                       if clicked.color != color:
+                          fill(clicked, grid, color, clicked.color)
+                          pygame.display.update()
+                                    
+                    else: 
+                        name = pygame.display.get_caption()[0]
+                        if name.find("*") < 1:
+                           changeCaption(name + '*')
+
+                        clicked = grid.clicked(pos)
+                        clicked.click(grid.screen,color)
+                        if thickness == 2:
+                            for pixel in clicked.neighbors:
+                                pixel.click(grid.screen, color)
+                        elif thickness == 3:
+                            for pixel in clicked.neighbors:
+                                pixel.click(grid.screen, color)
+                                for p in pixel.neighbors:
+                                    p.click(grid.screen, color)
+                        elif thickness == 4:
+                            for pixel in clicked.neighbors:
+                                pixel.click(grid.screen, color)
+                                for p in pixel.neighbors:
+                                    p.click(grid.screen, color)
+                                    for x in p.neighbors:
+                                        x.click(grid.screen, color)
+                                         
+                pygame.display.update()
+            except AttributeError:
+                pass
+
+pygame.quit()
